24da2e607576450bf7fb25a7c0d2b4a9adfcea6f
Alberto Medrano <albersna@hotmail.com>
2024-02-02 12:47:27 +0000

    Added: Review

--- core/designsystem/src/main/java/com/jerryalberto/mmas/core/designsystem/component/RoundedButton.kt
+++ core/designsystem/src/main/java/com/jerryalberto/mmas/core/designsystem/component/RoundedButton.kt
@@ -9,20 +9,19 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.jerryalberto.mmas.core.designsystem.theme.Orange40
+import com.jerryalberto.mmas.core.designsystem.theme.roundedShapeScheme
 
 @Composable
 fun RoundedButton(
@@ -31,10 +30,9 @@ fun RoundedButton(
     buttonColors: ButtonColors,
     iconLeft: ImageVector,
     title: String,
-    fontWeight: FontWeight,
-    fontStyle: FontStyle,
-    fontSize: TextUnit,
+    textStyle: TextStyle,
     padding: Dp,
+    contentDescription: String,
     onClick: () -> Unit = {}
 ) {
 
@@ -42,15 +40,13 @@ fun RoundedButton(
         onClick = onClick, shape = shape, border = border, colors = buttonColors
     ) {
         Icon(
-            imageVector = iconLeft, contentDescription = "Icon left"
+            imageVector = iconLeft, contentDescription = contentDescription
         )
         Text(
             modifier = Modifier.padding(start = padding),
             text = title,
             textAlign = TextAlign.Left,
-            fontWeight = fontWeight,
-            fontStyle = fontStyle,
-            fontSize = fontSize
+            style = textStyle
         )
     }
 
@@ -60,17 +56,16 @@ fun RoundedButton(
 @Composable
 private fun RoundedButtonPreview() {
     RoundedButton(
-        shape = RoundedCornerShape(10.dp),
+        shape = MaterialTheme.roundedShapeScheme.medium,
         border = BorderStroke(2.dp, Orange40),
         buttonColors = ButtonDefaults.buttonColors(
             contentColor = Color.White, containerColor = Orange40
         ),
         iconLeft = Icons.Default.DateRange,
         title = "Save",
-        fontWeight = FontWeight.Bold,
-        fontStyle = FontStyle.Italic,
-        fontSize = 20.sp,
+        textStyle = MaterialTheme.typography.titleMedium,
         padding = 5.dp,
+        contentDescription = "Icon left",
         onClick = {}
     )
 }
